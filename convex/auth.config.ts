49d8f8879150c10d0c946af87d2e7cb21e79865d
Kaveenxz <kaveenhansithx@gmail.com>
2025-03-09 20:30:36 +0530

    intergrate with clerk authendication

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+const authConfig = {
+    providers: [
+        {
+            domain: "https://actual-hare-80.clerk.accounts.dev",
+            applicationID: "convex"
+        }
+    ]
+}
+export default authConfig; 
