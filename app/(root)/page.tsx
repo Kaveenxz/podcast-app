65a9efb2097b4fa3266bdbbf91337ed2c69455d3
Kaveenxz <kaveenhansithx@gmail.com>
2025-03-09 18:05:41 +0530

    make the podcast card with props

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import PodcastCard from '@/components/PodcastCard'
 import { Button } from '@/components/ui/button'
 import { podcastData } from '@/constants'
 import React from 'react'
@@ -7,7 +8,15 @@ const page = () => {
     <div className='mt-9 flex flex-col gap-9'>
       <section className="flex flex-col gap-5">
         <h1 className="text-20 font-bold text-white-1">Trending Podcasts</h1>
-        
+        {podcastData.map((podcast) => (
+          <PodcastCard 
+            key={podcast.id}
+            imgURL={podcast.imgURL}
+            title={podcast.title}
+            description={podcast.description}
+            podcastId={podcast.id}
+          />
+        ))}
       </section>
     </div>
   )
---
+++ components/PodcastCard.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Image from 'next/image'
+const PodcastCard = ({imgURL, title, description, podcastId}: {
+    imgURL: string,
+    title: string,
+    description: string,
+    podcastId: number
+}) => {
+  return (
+    <div className='cursor-pointer'>
+        <figure className='flex flex-col gap-2'>
+            <Image src={imgURL} alt={title} width={174} height={174}></Image>
+            <h1 className='text-16 truncate font-bold text-white-1'>{title}</h1>
+            <h2 className='text-12 truncate font-normal capitalize text-white-4'>{description}</h2>
+        </figure>
+      
+    </div>
+  )
+}
+
+export default PodcastCard
