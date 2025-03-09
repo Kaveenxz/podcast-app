7210b8f5083eacd32a14d5f878d42e74dc9e9856
Kaveenxz <kaveenhansithx@gmail.com>
2025-03-09 19:22:26 +0530

    integrate convex and get data from it

--- components/PodcastCard.tsx
+++ components/PodcastCard.tsx
@@ -9,7 +9,7 @@ const PodcastCard = ({imgURL, title, description, podcastId}: {
   return (
     <div className='cursor-pointer'>
         <figure className='flex flex-col gap-2'>
-            <Image src={imgURL} alt={title} width={174} height={174}></Image>
+            <Image src={imgURL} alt={title} width={174} height={174} className='aspect-square h-fit w-full rounded-xl 2xl:size-[200px]'></Image>
             <h1 className='text-16 truncate font-bold text-white-1'>{title}</h1>
             <h2 className='text-12 truncate font-normal capitalize text-white-4'>{description}</h2>
         </figure>
